187940914368272a0229ef7a4382087c5de820c4
sptungG <ngoviettung154@gmail.com>
2024-11-11 06:27:50 +0700

    update playpage

--- src/pages/PlayPage.tsx
+++ src/pages/PlayPage.tsx
@@ -1,20 +1,19 @@
-import React, { useId } from "react";
+import { useId, useState } from "react";
 import { Tab, TabList, Tabs } from "react-aria-components";
-import { useAppSelector } from "../redux/store";
 import Prize01Svg from "../assets/prize-01.svg";
 import Prize02Svg from "../assets/prize-02.svg";
 import Prize03Svg from "../assets/prize-03.svg";
-import { useUrlState } from "../hooks/useUrlState";
 import { cn } from "../common/utils";
+import { useAppSelector } from "../redux/store";
 
 const PlayPage = () => {
   const uid = useId();
   const configState = useAppSelector((s) => s.config);
-  const [selectedTab, setSelectedTab] = useUrlState<{ prize?: string }>({ prize: "3RD" });
+  const [selectedTab, setSelectedTab] = useState<{ prize?: string }>({ prize: "3RD" });
 
   return (
     <div className="w-dvw h-dvh overflow-hidden">
-      <Tabs selectedKey={selectedTab.prize} onSelectionChange={(value) => setSelectedTab({ prize: value })}>
+      <Tabs selectedKey={selectedTab.prize} onSelectionChange={(value) => setSelectedTab({ prize: value as string })}>
         <TabList aria-label="Prize" className="flex flex-nowrap items-stretch">
           {configState.prizeConfig.map((item, index) => {
             const isSelected = item.type === selectedTab.prize;
